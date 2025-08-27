1daf1aa42154be4ac4fa6cb8bbba5443ac2e569e
P.Prasanth kumar <166972395+prasanth82006@users.noreply.github.com>
2025-08-27 15:24:19 +0530

    Update Book.js

--- models/Book.js
+++ models/Book.js
@@ -1,55 +1,3 @@
-// const mongoose = require('mongoose');
-
-// const bookSchema = new mongoose.Schema({
-//   title: {
-//     type: String,
-//     required: true,
-//     trim: true,
-//   },
-//   author: {
-//     type: String,
-//     required: true,
-//     trim: true,
-//   },
-//   category: {
-//     type: String,
-//     required: true,
-//     enum: ['fiction', 'non-fiction', 'fantasy', 'mystery', 'romance', 'science'],
-//   },
-//   condition: {
-//     type: String,
-//     required: true,
-//     enum: ['excellent', 'good', 'fair'],
-//   },
-//   description: {
-//     type: String,
-//     trim: true,
-//   },
-//   location: {
-//     type: String,
-//     required: true,
-//     trim: true,
-//   },
-//   image: {
-//     type: String,
-//     default: 'https://images.unsplash.com/photo-1544947950-fa07a98d237f?w=300&h=400&fit=crop',
-//   },
-//   userId: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: 'User',
-//     required: true,
-//   },
-//   postedDate: {
-//     type: Date,
-//     default: Date.now,
-//   },
-//   rating: {
-//     type: Number,
-//     default: 0,
-//   },
-// });
-
-// module.exports = mongoose.model('Book', bookSchema);
 
 const mongoose = require('mongoose');
 
@@ -106,5 +54,6 @@ const bookSchema = new mongoose.Schema({
     min: 0,
   },
 });
+
 
 module.exports = mongoose.model('Book', bookSchema);
