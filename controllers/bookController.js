f5187065520c2bb7c319f7cada404d454a79b221
P.Prasanth kumar <166972395+prasanth82006@users.noreply.github.com>
2025-08-27 15:21:24 +0530

    Update bookController.js

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,79 +1,3 @@
-// const Book = require('../models/Book');
-
-// exports.getBooks = async (req, res) => {
-//   try {
-//     const books = await Book.find().populate('userId', 'username');
-//     res.status(200).json(books);
-//   } catch (error) {
-//     res.status(500).json({ message: 'Server error' });
-//   }
-// };
-
-// exports.addBook = async (req, res) => {
-//   try {
-//     const { title, author, category, condition, description, location, image } = req.body;
-//     const userId = req.user.userId;
-
-//     const book = new Book({
-//       title,
-//       author,
-//       category,
-//       condition,
-//       description,
-//       location,
-//       image,
-//       userId,
-//     });
-
-//     await book.save();
-//     res.status(201).json(book);
-//   } catch (error) {
-//     res.status(500).json({ message: 'Server error' });
-//   }
-// };
-
-// const Book = require('../models/Book');
-
-// exports.getBooks = async (req, res) => {
-//   try {
-//     const books = await Book.find().populate('userId', 'username');
-//     res.status(200).json(books);
-//   } catch (error) {
-//     console.error('Error fetching books:', error);
-//     res.status(500).json({ message: 'Server error', error: error.message });
-//   }
-// };
-
-// exports.addBook = async (req, res) => {
-//   try {
-//     const { title, author, category, condition, description, location } = req.body;
-//     const userId = req.user.userId;
-//     const image = req.file ? `/uploads/${req.file.filename}` : req.body.image;
-
-//     // Validate required fields
-//     if (!title || !author || !category || !condition || !location) {
-//       return res.status(400).json({ message: 'All required fields must be provided' });
-//     }
-
-//     const book = new Book({
-//       title,
-//       author,
-//       category,
-//       condition,
-//       description,
-//       location,
-//       image,
-//       userId,
-//     });
-
-//     await book.save();
-//     res.status(201).json(book);
-//   } catch (error) {
-//     console.error('Error adding book:', error);
-//     res.status(500).json({ message: 'Server error', error: error.message });
-//   }
-// };
-
 
 const Book = require('../models/Book');
 
@@ -121,4 +45,5 @@ exports.addBook = async (req, res) => {
     console.error('Error adding book:', error);
     res.status(500).json({ message: 'Server error', error: error.message });
   }
+
 };
